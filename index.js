c13895af7f92c4b39506b54d7d7fba21a007a12e
acostal4 <luisacostaf4@gmail.com>
2025-04-18 11:36:05 -0500

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+// index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Mount the React app to the root div in index.html
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);

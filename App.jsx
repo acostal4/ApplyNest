8d58eafe872afd81eea73f0021e6992f0be7175c
acostal4 <luisacostaf4@gmail.com>
2025-04-18 11:37:55 -0500

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,25 +1,27 @@
 // App.jsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
 import Home from "./pages/Home";
-import SavedJobs from "./pages/SavedJobs";
 import Alerts from "./pages/Alerts";
-import AddAlertForm from "./pages/AddAlertForm";
+import AddAlertForm from "./components/AddAlertForm";
 import Profile from "./pages/Profile";
+import { UserProvider } from "./context/UserContext";
 
 const App = () => {
 return (
+<UserProvider>
 <Router>
-<Layout>
+<div className="min-h-screen bg-gray-100">
 <Routes>
 <Route path="/" element={<Home />} />
-<Route path="/saved" element={<SavedJobs />} />
 <Route path="/alerts" element={<Alerts />} />
 <Route path="/add-alert" element={<AddAlertForm />} />
 <Route path="/profile" element={<Profile />} />
 </Routes>
-</Layout>
+</div>
 </Router>
+</UserProvider>
 );
 };
+
+export default App;

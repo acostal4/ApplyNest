dc5f3f03db3f6390794465891e322a11d0d7c61d
acostal4 <luisacostaf4@gmail.com>
2025-04-18 19:10:17 -0500

    Profile.jsx

---
+++ Components/Profile.jsx
@@ -0,0 +1,10 @@
+const Profile = () => {
+return (
+<div>
+<h1 className="text-2xl font-bold mb-2">Your Profile</h1>
+<p>View or update your personal info here.</p>
+</div>
+);
+};
+
+export default Profile;

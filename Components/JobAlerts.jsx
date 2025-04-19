71d9ca5edd1c57041839de7a7ab1eccef5bde263
acostal4 <luisacostaf4@gmail.com>
2025-04-18 19:09:22 -0500

    JobAlerts.jsx

---
+++ Components/JobAlerts.jsx
@@ -0,0 +1,10 @@
+const JobAlerts = () => {
+return (
+<div>
+<h1 className="text-2xl font-bold mb-2">Your Job Alerts</h1>
+<p>All your current alerts will appear here.</p>
+</div>
+);
+};
+
+export default JobAlerts;

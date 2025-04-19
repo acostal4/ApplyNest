62bf5e97496b9ff30da379bf33d7b6c59629da3c
acostal4 <luisacostaf4@gmail.com>
2025-04-18 19:08:24 -0500

    AddAlert.jsx

---
+++ Components/AddAlert.jsx
@@ -0,0 +1,10 @@
+const AddAlert = () => {
+return (
+<div>
+<h1 className="text-2xl font-bold mb-2">Create a Job Alert</h1>
+<p>Use this page to set up job alert preferences.</p>
+</div>
+);
+};
+
+export default AddAlert;

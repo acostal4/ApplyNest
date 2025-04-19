b6dda86cb407448b8eb8f9110db95114ec7ce292
acostal4 <luisacostaf4@gmail.com>
2025-04-18 19:03:31 -0500

    App.jsx

--- App.jsx
+++ App.jsx
@@ -1,26 +1,31 @@
-// App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Alerts from "./pages/Alerts";
-import AddAlertForm from "./components/AddAlertForm";
-import Profile from "./pages/Profile";
-import { UserProvider } from "./context/UserContext";
+import { useState } from 'react';
+import Navbar from './components/Navbar';
 
 const App = () => {
+const [page, setPage] = useState('home');
+
+const renderPage = () => {
+switch (page) {
+case 'home':
+return <div>Welcome to ApplyNest!</div>;
+case 'add':
+return <div>This is the Add Alert page.</div>;
+case 'alerts':
+return <div>Here are your Job Alerts.</div>;
+case 'profile':
+return <div>This is your Profile page.</div>;
+default:
+return <div>Welcome to ApplyNest!</div>;
+}
+};
+
 return (
-<UserProvider>
-<Router>
-<div className="min-h-screen bg-gray-100">
-<Routes>
-<Route path="/" element={<Home />} />
-<Route path="/alerts" element={<Alerts />} />
-<Route path="/add-alert" element={<AddAlertForm />} />
-<Route path="/profile" element={<Profile />} />
-</Routes>
+<div>
+<Navbar onNavigate={setPage} />
+<main className="p-4">
+{renderPage()}
+</main>
 </div>
-</Router>
-</UserProvider>
 );
 };
 

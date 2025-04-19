392374ab2791ebc7e6ac15846880ab1de5cbaa7a
acostal4 <luisacostaf4@gmail.com>
2025-04-18 19:07:20 -0500

    Home.jsx

---
+++ Components/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+return (
+<div>
+<h1 className="text-2xl font-bold mb-2">Welcome to ApplyNest</h1>
+<p>Start finding and managing job opportunities with ease.</p>
+</div>
+);
+};
+
+export default Home;
